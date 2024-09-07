ac1cbeae035469fe8948bbf38b5ac0835865c9c8
Zachary Neeld <zacharyn25@gmail.com>
2024-09-07 16:08:39 -0400

    Added a clock to only update screen at 60 fps

--- main.py
+++ main.py
@@ -8,6 +8,10 @@ def main():
     #Initialze the pygame
     pygame.init()
 
+    #Create Clock Object 
+    clock = pygame.time.Clock()
+    dt = 0 #delta time
+
     #Create display screen
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
@@ -19,9 +23,13 @@ def main():
             
         #Fills the pygame display with a black background
         screen.fill((0,0,0))
-        
+
         #Updates the pygame display each iteration
         pygame.display.flip()
+
+        #Update at 60 FPS
+        time_since_called = clock.tick(60) #(s)
+        dt = time_since_called / 1000 #(ms)
 
 if __name__ == "__main__":
     main()
